2ba4ac137d9ef68ac96e79486cc4cb9d37bc21a0
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-07 16:23:39 +0530

    Sending JSON obj from req and sending to the FE

--- src/main/java/lk/ijse/collector/NoteController.java
+++ src/main/java/lk/ijse/collector/NoteController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/notes")
 public class NoteController {
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
     public NoteDTO saveNote(@RequestBody NoteDTO noteDTO){
         noteDTO.setNoteId(AppUtill.generateNoteId());
         return noteDTO;
