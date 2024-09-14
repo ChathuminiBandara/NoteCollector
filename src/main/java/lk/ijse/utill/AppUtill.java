8206973a5e1d9b710259b32179a6223ca36ec887
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-14 18:28:31 +0530

    generating UUID for users

--- src/main/java/lk/ijse/utill/AppUtill.java
+++ src/main/java/lk/ijse/utill/AppUtill.java
@@ -1,5 +1,6 @@
 package lk.ijse.utill;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtill {
@@ -7,4 +8,10 @@ public class AppUtill {
     return  "Note-" + UUID.randomUUID();
 
     }
+
+    public static String generateUserId(){
+        return "User-" + UUID.randomUUID();
+    }
+
+   
 }
