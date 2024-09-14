afbf3f35cd53717885f30bc9c990e517b8ec6565
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-14 18:33:07 +0530

    Base64 encoder : ProfilePic

--- src/main/java/lk/ijse/utill/AppUtill.java
+++ src/main/java/lk/ijse/utill/AppUtill.java
@@ -13,5 +13,7 @@ public class AppUtill {
         return "User-" + UUID.randomUUID();
     }
 
-   
+    public static String profilePicToBase64(String profilePic){
+        return Base64.getEncoder().encodeToString(profilePic.getBytes());
+    }
 }
