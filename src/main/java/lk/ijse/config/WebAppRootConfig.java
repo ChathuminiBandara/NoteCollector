6c734cb52ebb3073c159fc7aa2dc2f74d25edb04
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-14 14:54:05 +0530

    DriverManagerDataSource added :  DataSource

--- src/main/java/lk/ijse/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/config/WebAppRootConfig.java
@@ -1,9 +1,60 @@
 package lk.ijse.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.val;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse")
+@EnableTransactionManagement
+@EnableJpaRepositories
 public class WebAppRootConfig {
+
+    //data source eka defined
+    @Bean
+    public DataSource dataSource() {
+        //new data source : add
+        var dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/springNote?createDatabaseIfNotExist=true&useSSL=false");
+        dmds.setUsername("root");
+        dmds.setPassword("root");
+        return dmds;
+    }
+
+    //hibernate JPA Implementation
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.acme.domain");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    //to manage the transactions
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 }
