289bbba9c87a673d2e5b35112bc369fa154ecfd9
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-14 10:07:42 +0530

    Dependency Injection : NoteController

--- src/main/java/lk/ijse/collector/NoteController.java
+++ src/main/java/lk/ijse/collector/NoteController.java
@@ -1,7 +1,9 @@
 package lk.ijse.collector;
 
+import lk.ijse.Service.NoteServiceIMPL;
 import lk.ijse.dto.impl.NoteDTO;
 import lk.ijse.utill.AppUtill;
+import lombok.val;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +16,27 @@ import java.util.List;
 @RequestMapping("api/v1/notes")
 public class NoteController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
+
+    //controller layer eke endpoints - mewa equal wenawa crud operations walata - all are public
     public NoteDTO saveNote(@RequestBody NoteDTO noteDTO){
+        var noteServiceIMPL = new NoteServiceIMPL();
         noteDTO.setNoteId(AppUtill.generateNoteId());
+        noteServiceIMPL.saveNote(noteDTO);
         return noteDTO;
     }
+
     public String getSelectedNote(){
         return null;
     }
+
     public List<NoteDTO> getAllNotes(){
         return null;
     }
+
     public void deleteNote(String noteId){
 
     }
+
     public void updateNte(String noteId, NoteDTO noteDTO){
 
     }
