332c9f6f5c9d6a0c3545d64fd0322f33e131fda7
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-14 10:34:47 +0530

    App Init

--- src/main/java/lk/ijse/AppInit.java
+++ src/main/java/lk/ijse/AppInit.java
@@ -18,6 +18,6 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{};
+        return new String[]{"/"};
     }
 }
