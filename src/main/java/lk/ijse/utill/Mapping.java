c8b5479e6b0e39cd9bb65cb96fedb5ca86983e0d
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-28 12:51:09 +0530

    model mappers : conversion

---
+++ src/main/java/lk/ijse/utill/Mapping.java
@@ -0,0 +1,23 @@
+package lk.ijse.utill;
+
+import lk.ijse.dto.impl.UserDTO;
+import lk.ijse.entity.impl.UserEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // for user mapping
+    public UserEntity toUserEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
+
+    public UserDTO toUserDTO(UserEntity userEntity){
+        return modelMapper.map(userEntity, UserDTO.class);
+    }
+
+}
