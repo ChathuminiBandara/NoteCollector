68902181300e2fa9c49e6a04130806472f4b8561
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-28 12:26:35 +0530

    User DAO : just dao ne implementation of impl

--- src/main/java/lk/ijse/dao/UserDAO.java
+++ src/main/java/lk/ijse/dao/UserDAO.java
@@ -5,5 +5,5 @@ import lk.ijse.entity.impl.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO extends JpaRepository<UserEntity, String> {  // entity type and primality keys daya type{
-    UserEntity saveUser(UserDTO userDTO);
+
 }
