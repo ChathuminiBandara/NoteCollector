bcb3e4680346c37ad95298dd18acf9c29517a6b7
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-28 12:52:12 +0530

    conversion methods

--- src/main/java/lk/ijse/utill/Mapping.java
+++ src/main/java/lk/ijse/utill/Mapping.java
@@ -11,11 +11,12 @@ public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
 
-    // for user mapping
+    // for user mapping : conversion from entity to dto
     public UserEntity toUserEntity(UserDTO userDTO){
         return modelMapper.map(userDTO, UserEntity.class);
     }
 
+    // conversion dto to entity
     public UserDTO toUserDTO(UserEntity userEntity){
         return modelMapper.map(userEntity, UserDTO.class);
     }
