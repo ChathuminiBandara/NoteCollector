ac872df084d51ed0ecdf605f2ea283e29fedfaf7
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-30 23:18:12 +0530

    Configs for appconfig

--- src/main/java/lk/ijse/config/WebAppConfig.java
+++ src/main/java/lk/ijse/config/WebAppConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = "lk.ijse")
 @EnableWebMvc
-@MultipartConfig   // multipart configuration enabling
+@MultipartConfig(   // multipart configuration enabling
+        fileSizeThreshold = 1024*1024*2, //
+        maxFileSize = 1024*1024*5, //5mb must be in bits
+        maxRequestSize = 1024*1024*10  //total size of the request including file sizes
+)
 public class WebAppConfig {
 }
