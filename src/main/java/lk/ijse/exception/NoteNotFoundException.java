af8a3af929cc397d1b48eadb8a18142aac6cc2c5
ChathuminiBandara <chathubandara500@gmail.com>
2024-09-30 23:17:20 +0530

    Custom Exceptions

---
+++ src/main/java/lk/ijse/exception/NoteNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.exception;
+
+public class NoteNotFoundException extends RuntimeException {
+    public NoteNotFoundException() {
+
+    }
+
+    public NoteNotFoundException(String message) {
+
+    }
+
+    public NoteNotFoundException(String message, Throwable cause) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package lk.ijse.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+
+    }
+}
